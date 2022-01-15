0d3202c4879bfbf3c3fbcd73a68b281fba7e1041
Charlie <64078329+zacharlie@users.noreply.github.com>
2022-01-15 23:31:19 +0200

    remove js module mimetypes

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,6 @@ from flask_appbuilder import AppBuilder, SQLA
 
 from app.index import DefaultIndexView
 
-"""
- add js modules support
-"""
-
-# import mimetypes
-
-# mimetypes.add_type("application/javascript", ".js")
-# mimetypes.add_type("application/javascript", ".mjs")
 
 """
  Logging configuration
